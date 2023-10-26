f22670e19fdafe11cc5a9884cf4960447ad0e12d
EuniceHwang <appletart177@gmail.com>
2023-10-27 00:19:21 +0900

    초기 커밋.

---
+++ Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectSetup
+{
+    public class Project
+    {
+        public string Property1 { get; set; }
+        public string Property2 { get; set; }
+        public DateTime ProjectTime { get; set; }
+
+    }
+}
